11c9207a30128d09cafac3e5765710df4f3f55f6
diegofa85 <diegofa85@educastur.es>
2024-11-26 19:42:18 +0100

    Programa4-v1.0

--- src/Main.kt
+++ src/Main.kt
@@ -2,62 +2,59 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    println("Introduce la Expresión: ")
-    var expresion = readLine()!!
-    val expresionList: MutableList<Char> = mutableListOf()
+    val acciones = arrayOf("correr", "saltar", "correr", "saltar", "correr")
+    var pista: String = "_|_|_"
+    var pistaA: MutableList<Char> = mutableListOf()
 
-    for (posicion in expresion) {
-        if (posicion == '('
-            || posicion == ')'
-            || posicion == '['
-            || posicion == ']'
-            || posicion == '{'
-            || posicion == '}') {
-            expresionList.add(posicion)
+    for (posicion in pista) {
+        pistaA.add(posicion)
+    }
+
+    var r: Boolean = true
+    var i: Int = 0
+    var a: Int = 0
+
+    if (acciones.size < pistaA.size) {
+        val s: Int = pistaA.size - acciones.size
+        a = pistaA.size - s
+        while(a < pistaA.size) {
+            pistaA.set(a,'?');
+            a++
+            r = false
         }
     }
 
-    var i = 0
-    while (i < expresionList.size) {
-        if (expresionList[i] == ')') {
-            if (i > 0 && expresionList[i - 1] == '(') {
-                expresionList.removeAt(i)
-                expresionList.removeAt(i - 1)
-                i = i - 1
-            } else {
-                println("$expresion ¿está balanceada?")
-                println("false")
-                return
-            }
-        } else if (expresionList[i] == ']') {
-            if (i > 0 && expresionList[i - 1] == '[') {
-                expresionList.removeAt(i)
-                expresionList.removeAt(i - 1)
-                i = i - 1
-            } else {
-                println("$expresion ¿está balanceada?")
-                println("false")
-                return
-            }
-        } else if (expresionList[i] == '}') {
-            if (i > 0 && expresionList[i - 1] == '{') {
-                expresionList.removeAt(i)
-                expresionList.removeAt(i - 1)
-                i = i - 1
-            } else {
-                println("$expresion ¿está balanceada?")
-                println("false")
-                return
-            }
+    if (pistaA.size < acciones.size) {
+        val s: Int = acciones.size - pistaA.size
+        a = acciones.size - s
+        while(a < acciones.size) {
+            pistaA.set(a,'?');
+            a++
+            r = false
+        }
+    }
+
+    while (i < acciones.size) {
+        if (acciones[i].equals("correr") && pistaA[i] == '_') {
+
+        } else if (acciones[i].equals("correr") && pistaA[i] == '|') {
+            pistaA.set(i,'/');
+            r = false
+        } else if (acciones[i].equals("saltar") && pistaA[i] == '_') {
+            pistaA.set(i,'x');
+            r = false
+        } else if (acciones[i].equals("saltar") && pistaA[i] == '|') {
+
         } else {
-            i++
+            pistaA.set(i,'?');
+            r = false
         }
+        i++
     }
 
-    println("$expresion ¿está balanceada?")
-    if (expresionList.isEmpty()) {
-        println("true")
-    } else {
-        println("false")
+    for (posicion in pistaA.indices){
+        print(pistaA.get(posicion))
     }
+    println(" ")
+    println(r)
 }
